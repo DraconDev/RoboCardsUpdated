1f48c4cc328de4a6a8274099efafc559821ba3b0
unknown <dracsharp@gmail.com>
2020-04-03 08:34:54 +0100

    fixed chrome devtools bug

--- src/index.js
+++ src/index.js
@@ -5,36 +5,53 @@ import * as serviceWorker from "./serviceWorker";
 import "tachyons";
 import App from "./containers/App.jsx";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware, combineReducers } from "redux";
+import { createStore, applyMiddleware, combineReducers, compose } from "redux";
 import { searchRobots, requestRobots } from "./reducers";
 import thunkMiddleware from "redux-thunk";
 import { createLogger } from "redux-logger";
-import { compose } from "redux";
+// import { compose } from "redux";
+// import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+// console.log(process.env.NODE_ENV);
+// const sagaMiddleware = createSagaMiddleware();
 
 const logger = createLogger();
+const initialState = {};
+const middlewares = [
+  logger,
+  thunkMiddleware,
+  // , sagaMiddleware
+];
 
 // Combine actions
 const rootReducer = combineReducers({ searchRobots, requestRobots });
 
-//
-// const middlewares = [thunkMiddleware, logger];
+const devTools =
+  process.env.NODE_ENV === "production"
+    ? applyMiddleware(...middlewares)
+    : composeWithDevTools(applyMiddleware(...middlewares));
 
-//Check if it's chrome
-const isChrome =
-  !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
-console.log("store.js", isChrome);
+export const store = createStore(rootReducer, initialState, devTools);
 
-// Combine Middleware and Chrome Dev tools. Else it doesn't work
-const middleWare = isChrome
-  ? compose(
-      applyMiddleware(thunkMiddleware, logger),
-      window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
-  : applyMiddleware(thunkMiddleware, logger);
+// //
+// // const middlewares = [thunkMiddleware, logger];
 
-//Store
-const store = createStore(rootReducer, middleWare);
+// //Check if it's chrome
+// const isChrome =
+//   !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
+// console.log("store.js", isChrome);
+
+// // Combine Middleware and Chrome Dev tools. Else it doesn't work
+// const middleWare = isChrome
+//   ? compose(
+//       applyMiddleware(thunkMiddleware, logger),
+//       window.__REDUX_DEVTOOLS_EXTENSION__ &&
+//         window.__REDUX_DEVTOOLS_EXTENSION__()
+//     )
+//   : applyMiddleware(thunkMiddleware, logger);
+
+// //Store
+// const store = createStore(rootReducer, middleWare);
 
 //Render
 ReactDOM.render(
